189e0352397d97679c8a31386beb0be99b95c350
Beatrix Haddock <beatrixh@gen-uge-submit-p01.hosts.ihme.washington.edu>
2019-12-10 14:28:00 -0800

    added boilerplate files

---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+
+if __name__ == "__main__":
+
+    base_dir = Path(__file__).parent
+    src_dir = base_dir / 'src'
+
+    about = {}
+    with (src_dir / "special_package" / "__about__.py").open() as f:
+        exec(f.read(), about)
+
+    #with (base_dir / "README.rst").open() as f:
+    #    long_description = f.read()
+
+    install_requirements = [
+
+    ]
+
+    interactive_requirements = [
+
+    ]
+
+    test_requirements = [
+
+    ]
+
+    doc_requirements = [
+
+    ]
+
+    setup(
+        name=about['__title__'],
+        version=about['__version__'],
+
+        description=about['__summary__'],
+        long_description=long_description,
+        license=about['__license__'],
+        url=about["__uri__"],
+
+        author=about["__author__"],
+        author_email=about["__email__"],
+
+        package_dir={'': 'src'},
+        packages=find_packages(where='src'),
+        
+        include_package_data=True,
+
+        install_requires=install_requirements,
+        tests_require=test_requirements,
+        extras_require={
+            'docs': doc_requirements,
+            'test': test_requirements,
+            'interactive': interactive_requirements,
+            'dev': doc_requirements + test_requirements + interactive_requirements,
+        },
+
+
+        zip_safe=False,
+    )
---
+++ src/special_package/__about__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", 
+]
+
+__title__ = "vivarium"
+__summary__ = "vivarium is a microsimulation framework built on top of the standard scientific python stack."
+__uri__ = "https://github.com/ihmeuw/vivarium"
+
+__version__ = "1.0.0"
+
+__author__ = "Beatrix Haddock"
+__email__ = "beatrixh@uw.com"
